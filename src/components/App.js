20bf565935c745c7460a26964efc3eefb1d21ecb
Adam Aaronson <adamaaronson@gmail.com>
2022-02-26 12:14:46 -0600

    Create basic Spotify authorizer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { SpotifyApiContext } from 'react-spotify-api'
+import Cookies from 'js-cookie'
+
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css'
+
 
 export default function App() {
+  const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
+
   return (
-    <h1>Name That Tune!</h1>
+    <div className='app'>
+      {token ? (
+        <SpotifyApiContext.Provider value={token}>
+          {/* Your Spotify Code here */}
+          <p>You are authorized with token: {token}</p>
+        </SpotifyApiContext.Provider>
+      ) : (
+        // Display the login page
+        <SpotifyAuth
+          redirectUri='http://localhost:3000/callback'
+          clientID='29c1316b4c844ab3b3abb8a9fff67957'
+          scopes={[Scopes.userReadPrivate, Scopes.userLibraryRead]} // either style will work
+          onAccessToken={(token) => setToken(token)}
+        />
+      )}
+    </div>
   )
 }
